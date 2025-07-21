7427927a715e1b8f98755db6c702fd46c499ba09
sooah1202 <001202s@gmail.com>
2025-07-21 16:57:58 +0900

    Collections.synchronizedList vs CopyOnWriteArrayList 반복 안정성 테스트 추가

---
+++ src/test2_syncListTest_vs_COWListTest/CopyOnWriteArrayList.java
@@ -0,0 +1,24 @@
+package test2_syncListTest_vs_COWListTest;
+
+public class CopyOnWriteArrayList {
+	public static void main(String[] args) {
+		java.util.concurrent.CopyOnWriteArrayList<String> list = new java.util.concurrent.CopyOnWriteArrayList<>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+
+		new Thread(() -> {
+			try {
+				Thread.sleep(10);
+				list.add("D");
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		for(String s : list) {
+			System.out.println(s);
+			try { Thread.sleep(50); } catch(InterruptedException e) {}
+		}
+	}
+}
---
+++ src/test2_syncListTest_vs_COWListTest/SyncListTest.java
@@ -0,0 +1,30 @@
+package test2_syncListTest_vs_COWListTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SyncListTest {
+
+	public static void main(String[] args) {
+		List<String> list = Collections.synchronizedList(new ArrayList<>());
+		list.add("A");
+		list.add("B");
+		list.add("C");
+
+		new Thread(() -> {
+			try {
+				Thread.sleep(10);
+				list.add("D");
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		for(String s : list) {
+			System.out.println(s);
+			try { Thread.sleep(50); } catch(InterruptedException e) {}
+		}
+	}
+}
