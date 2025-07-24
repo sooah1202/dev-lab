7aa428edf467d42734bc2b4a7a00cf467b18134a
sooah1202 <001202s@gmail.com>
2025-07-24 09:30:45 +0900

    test: wait(), notify(), notifyAll() 사용 방식 및 상태 전이 실습 코드 추가

---
+++ src/thread/ThreadStateTest_Notify.java
@@ -0,0 +1,49 @@
+package thread;
+
+public class ThreadStateTest_Notify {
+
+	private static final Object lock = new Object();
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(new MyThread(), "thread1");
+		Thread t2 = new Thread(new MyThread(), "thread2");
+		Thread t3 = new Thread(new MyThread(), "thread3");
+
+		t1.start();
+		t2.start();
+		t3.start();
+
+		Thread.sleep(1000);
+
+		synchronized(lock) {
+			System.out.println("[Main notify()]");
+			lock.notify();
+		}
+
+		Thread.sleep(1000);
+
+		synchronized(lock) {
+			System.out.println("[Main notifyAll()]");
+			lock.notifyAll();
+		}
+	}
+
+	static class MyThread implements Runnable {
+		@Override
+		public void run()
+		{
+			synchronized(lock) {
+				try{
+					System.out.println("[" + Thread.currentThread().getName() + "] waiting...");
+					lock.wait();
+					System.out.println("[" + Thread.currentThread().getName() + "] notified and running!");
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
+
